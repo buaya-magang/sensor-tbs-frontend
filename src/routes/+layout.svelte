a279a26a73dcb8fdb76d63a9d537df13b944c81d
buayaMAGANG <buayamagang13@gmail.com>
2025-08-08 16:13:08 +0700

    Footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,10 @@
-<script lang="ts">
-	import '../app.css'; // Sesuaikan jika Anda menggunakan metode CSS manual
-	import Navbar from '$lib/components/Navbar.svelte';
+<script>
+    import Navbar from '$lib/components/Navbar.svelte';
 </script>
 
-<div class="flex min-h-screen flex-col bg-slate-50 font-sans">
-	<Navbar />
-
-	<main class="flex-grow">
-		<slot />
-	</main>
-
-	<Footer /> </div>
+<div class="min-h-screen bg-slate-50 font-sans">
+    <Navbar></Navbar>
+    <main>
+        <slot></slot>
+    </main>
+</div>
