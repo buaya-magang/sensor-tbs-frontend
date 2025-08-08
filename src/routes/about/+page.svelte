6d86d39f920aee987aa48027e6875a196913dded
buayaMAGANG <buayamagang13@gmail.com>
2025-08-08 14:59:28 +0700

    FileUpload untuk bisa menampilkan gambar yg diprediksi

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -9,13 +9,10 @@
 	
 	<div class="order-2 flex flex-col justify-center space-y-6 text-center lg:order-1 lg:text-left">
 		<h1 use:viewport class="fade-up text-4xl font-bold text-gray-800 lg:text-5xl">
-			Tentang SensorTBS
+			SensorTBS
 		</h1>
 		<p use:viewport class="fade-up text-gray-600" style="transition-delay: 150ms;">
-			Aplikasi ini bertujuan untuk meningkatkan efisiensi dan akurasi dalam klasifikasi TBS, yang pada gilirannya akan membantu pengelolaan dan pengambilan keputusan yang lebih baik dalam pertanian kelapa sawit.
-		</p>
-		<p use:viewport class="fade-up text-gray-600" style="transition-delay: 300ms;">
-			Pengguna dapat mengunggah gambar TBS. Prediksi dilakukan menggunakan model pembelajaran mesin yang telah dilatih berdasarkan data gambar yang diambil langsung dari lapangan.
+			Aplikasi ini bertujuan untuk meningkatkan efisiensi dan akurasi dalam menentukan tingkat kematangan TBS Kelapa Sawit, yang pada gilirannya akan membantu pengelolaan dan pengambilan keputusan yang lebih baik dalam pertanian kelapa sawit.
 		</p>
 		<div use:viewport class="fade-up flex flex-col items-center sm:flex-row sm:justify-center lg:justify-start" style="transition-delay: 450ms;">
 			<a
