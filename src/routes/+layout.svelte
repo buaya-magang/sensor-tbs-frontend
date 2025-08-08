49640846517a6d8b6c50b98b5df1478834985964
buayaMAGANG <buayamagang13@gmail.com>
2025-08-08 13:40:15 +0700

    layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,10 @@
-<script>import "../app.css";
-import Navbar from '$lib/components/Navbar.svelte';</script>
+<script>
+    import Navbar from '$lib/components/Navbar.svelte';
+</script>
 
 <div class="min-h-screen bg-slate-50 font-sans">
-	<Navbar></Navbar>
-	<main>
-		<slot></slot>
-	</main>
+    <Navbar></Navbar>
+    <main>
+        <slot></slot>
+    </main>
 </div>
