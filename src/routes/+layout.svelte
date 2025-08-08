2cdfbd773a6fb4e924ca2a7ebcab47cc343ba420
buayaMAGANG <buayamagang13@gmail.com>
2025-08-08 15:51:58 +0700

    Footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,14 @@
-<script>
-    import Navbar from '$lib/components/Navbar.svelte';
+<script lang="ts">
+	import '../app.css'; // Sesuaikan jika Anda menggunakan metode CSS manual
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Footer from '$lib/components/Footer.svelte'; // 1. Impor komponen Footer
 </script>
 
-<div class="min-h-screen bg-slate-50 font-sans">
-    <Navbar></Navbar>
-    <main>
-        <slot></slot>
-    </main>
-</div>
+<div class="flex min-h-screen flex-col bg-slate-50 font-sans">
+	<Navbar />
+
+	<main class="flex-grow">
+		<slot />
+	</main>
+
+	<Footer /> </div>
