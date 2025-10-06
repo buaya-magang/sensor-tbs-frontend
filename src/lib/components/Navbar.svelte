334650f41331e873a44d8ccd592cadefd592657e
buayaMAGANG <buayamagang13@gmail.com>
2025-10-06 12:14:28 +0700

    fix: navbar

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,10 +1,10 @@
-<header class="bg-slate-50/80 backdrop-blur-lg sticky top-0 z-10">
-	<nav class="container mx-auto flex items-center justify-between p-4">
-		<a href="/" class="text-2xl font-bold text-blue-900">SensorTBS</a>
-		<ul class="flex items-center space-x-6 font-medium text-gray-700">
-			<li><a href="/" class="hover:text-blue-600">Home</a></li>
-			<li><a href="/klasifikasi" class="hover:text-blue-600">Classification</a></li>
-			<li><a href="/about" class="hover:text-blue-600">About</a></li>
-		</ul>
-	</nav>
+<header class="sticky top-0 z-10 bg-slate-50/80 backdrop-blur-lg" style="margin-left: 30px; -webkit-text-size-adjust: 100%; tab-size: 4; font-feature-settings: normal; font-variation-settings: normal; -webkit-tap-highlight-color: transparent; line-height: inherit; font-family: ui-sans-serif, system-ui, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'; box-sizing: border-box; border-width: 0; border-style: solid; border-color: #e5e7eb;">
+    <nav class="container mx-auto flex items-center justify-between p-4">
+        <a href="/" class="text-2xl font-bold text-blue-900">SensorTBS</a>
+        <ul class="flex items-center space-x-6 font-medium text-gray-700">
+            <li><a href="/" class="hover:text-blue-600">Home</a></li>
+            <li><a href="/klasifikasi" class="hover:text-blue-600">Classification</a></li>
+            <li><a href="/about" class="hover:text-blue-600">About</a></li>
+        </ul>
+    </nav>
 </header>
